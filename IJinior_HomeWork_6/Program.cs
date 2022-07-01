3929dd119ab06cc1182a873dd3d8e320171e77ee
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2022-07-01 16:52:24 +0300

    First commit

---
+++ IJinior_HomeWork_6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IJinior_HomeWork_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int picturesInRow = 3;
+            int fullRows = pictures / picturesInRow;
+            int extraPictures = pictures % picturesInRow;
+            Console.WriteLine($"Заполненные ряды - {fullRows}. Лишние картинки - {extraPictures}");
+        }
+    }
+}
